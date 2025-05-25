4d6fff5e1a57a9d25161c89ea71e005c8c916cb2
Geetisha17 <geetishatandon@gmail.com>
2025-05-25 16:24:19 +0530

    debugging issues

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -14,5 +14,8 @@ router.delete("/delete", deleteChat);
 router.post("/save", saveChat);
 router.post("/message", sendMessage);
 router.post("/image", generateImage);
+router.get("/",(req,res)=>{
+  res.send("Chat route is working");
+})
 
 export default router;
