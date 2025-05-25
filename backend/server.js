7bcb19cf1b51bac3ba5b407ffe950f081ceac226
Geetisha17 <geetishatandon@gmail.com>
2025-05-25 17:18:18 +0530

    cors origin added

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import chatRoutes from "./routes/chatRoutes.js";
 
 const app = express();
 app.use(cors({
-  origin: ["http://localhost:3000","http://174.129.135.117:5000", "http://localhost:5173" , "https://syn-chat-l0gwxnkpq-geetishatandongmailcoms-projects.vercel.app/" , "https://syn-chat.vercel.app/"],
+  origin: ["http://localhost:3000","http://174.129.135.117:5000", "http://localhost:5173"  , "http://ai-chat-bot-application.s3-website-us-east-1.amazonaws.com/chat"],
   methods: ['GET', 'POST', "PUT", "DELETE", "OPTIONS"],
   credentials:true
 }));
